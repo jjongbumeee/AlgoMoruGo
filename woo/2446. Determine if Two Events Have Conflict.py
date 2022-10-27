c413cd60bff7291cc6aa1fcb568f0cec75178527
queenwoo <114654362+queenwoo@users.noreply.github.com>
2022-10-27 15:35:33 +0900

    Update 2446. Determine if Two Events Have Conflict.py

--- woo/2446. Determine if Two Events Have Conflict.py
+++ woo/2446. Determine if Two Events Have Conflict.py
@@ -2,4 +2,3 @@ class Solution(object):
     def haveConflict(self, event1, event2):
         convert = lambda x: int(x.split(':')[0]) * 60 + int(x.split(':')[1])
         return set(range(convert(event1[0]), convert(event1[1]) + 1)) & set(range(convert(event2[0]), convert(event2[1]) + 1))
-        
