37b666db81257caae2e4483de6eb51b1cdb69610
queenwoo <114654362+queenwoo@users.noreply.github.com>
2022-10-31 14:44:47 +0900

    Update 2455. Average Value of Even Numbers That Are Divisible by Three.py

--- woo/2455. Average Value of Even Numbers That Are Divisible by Three.py
+++ woo/2455. Average Value of Even Numbers That Are Divisible by Three.py
@@ -1,4 +1,4 @@
 class Solution(object):
     def averageValue(self, nums):
         nums = [num for num in nums if num % 6 == 0]
-        return int(sum(nums) // len(nums)) if nums else 0
+        return sum(nums) // len(nums) if nums else 0
